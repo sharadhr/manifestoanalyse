e06f2a2121bb8c60e2ea5d7b60f189b216bfd2b9
karth021 <59319354+karth021@users.noreply.github.com>
2020-07-02 16:02:03 +0800

    Integrated string splitter into main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,20 @@ import java.io.OutputStreamWriter;
 
 
 public class Main {
+
+    private static String[] macro_split(String text){
+        String[] split_strings = text.split("(\\w+)");
+        return split_strings;
+    }
+
     public static void main(String[] args) {
         // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Request for user input
-        System.out.println(PdfUtil.parsePdfFromDoc(PdfUtil.loadPDF(PdfUtil.getFile(PdfUtil.getPdfPath()))));
-
+        String NSP_manifesto = PdfUtil.parsePdfFromDoc(PdfUtil.loadPDF(PdfUtil.getFile(PdfUtil.getPdfPath())));
+        String[] words = macro_split(NSP_manifesto);
+        System.out.println(words);
         System.exit(0);
     }
 }
